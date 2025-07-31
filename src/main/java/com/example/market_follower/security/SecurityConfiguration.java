2b1b257b1d61648549c5728ff4b14d29856a1554
seongjun-working-directories <seongjuncode@gmail.com>
2025-07-31 23:28:52 +0900

    |FEAT| SecurityConfiguration으로 Swagger API 미인가 접근 허용

---
+++ src/main/java/com/example/market_follower/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.market_follower.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth-> auth
+                        // 정적 리소스는 접근을 항상 허용
+                        .requestMatchers("/css/**", "/js/**", "/image/**", "/images/**", "/webjars/**").permitAll()
+
+                        .requestMatchers("/", "/swagger-ui/index.html").permitAll()
+
+                        .requestMatchers("/signup").permitAll()
+
+                        .requestMatchers("/member/**").hasAuthority("ROLE_ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())  // Authorization: Basic base64(username:password) 형식으로 인증하겠다는 선언
+                .headers(headers -> headers
+                                .frameOptions(frameOptionsConfig -> frameOptionsConfig.sameOrigin())
+                );
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
