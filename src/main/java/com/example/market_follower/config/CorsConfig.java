d154fc69ba962033ee61a292aa5b1545f0f4e3ba
seongjun-working-directories <seongjuncode@gmail.com>
2025-08-11 18:31:13 +0900

    |FEAT| CORS 추가

---
+++ src/main/java/com/example/market_follower/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.market_follower.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // 또는 허용할 도메인 명시
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
