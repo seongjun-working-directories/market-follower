06f214eb4d9aa4fffeae1365c9f61d5acdb9bcc3
seongjun-working-directories <seongjuncode@gmail.com>
2025-08-12 20:26:09 +0900

    |FEAT| 데이터 및 스키마 초기화

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
--- auth 테이블 초기 데이터
-INSERT INTO auth (id) VALUES (1);
-INSERT INTO auth (id) VALUES (2);
-
 -- member 테이블 초기 데이터
 INSERT INTO member (name, email, phone_number, birthday, created_at, last_login_at)
-VALUES ('홍길동', 'hong@example.com', '01012345678', '1990-01-01', NOW(), NOW());
+VALUES ('홍길동', 'hong@example.com', '01012345678', DATE '1990-01-01', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO member (name, email, phone_number, birthday, created_at, last_login_at)
-VALUES ('김영희', 'kim@example.com', '01087654321', '1995-05-15', NOW(), NOW());
+VALUES ('김영희', 'kim@example.com', '01087654321', DATE '1995-05-15', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+-- auth 테이블 초기 데이터
+INSERT INTO auth (member_id, role) VALUES (1, 'ROLE_USER');
+INSERT INTO auth (member_id, role) VALUES (2, 'ROLE_ADMIN');
