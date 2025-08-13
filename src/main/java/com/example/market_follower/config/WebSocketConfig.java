b93e915f1414d220ba88f7928d884d3541ad31c0
seongjun-working-directories <seongjuncode@gmail.com>
2025-08-13 22:49:05 +0900

    |FIX| STOMP 하트비트 설정으로 WebSocket 연결 유지를 위한 TaskScheduler 설정

--- src/main/java/com/example/market_follower/config/WebSocketConfig.java
+++ src/main/java/com/example/market_follower/config/WebSocketConfig.java
@@ -4,6 +4,7 @@ import com.example.market_follower.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,8 +17,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        // TaskScheduler 생성
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        taskScheduler.initialize();
+
+        // SimpleBroker에 하트비트와 스케줄러 연결
         config.enableSimpleBroker("/topic")
-                .setHeartbeatValue(new long[]{10000, 10000});    // 구독 prefix
+                .setHeartbeatValue(new long[]{10000, 10000})
+                .setTaskScheduler(taskScheduler);               // 구독 prefix
+
+        // 클라이언트 -> 서버로 메시지 prefix
         config.setApplicationDestinationPrefixes("/app");       // 발행 prefix
     }
 
