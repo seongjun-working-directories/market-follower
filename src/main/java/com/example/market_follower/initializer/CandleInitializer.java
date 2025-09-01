e3c6e97a1a167380a27a6043231a7856a7849b76
seongjun-working-directories <seongjuncode@gmail.com>
2025-09-01 23:39:09 +0900

    |FIX| 서버 실행 시 데이터 셋업 코드를 시간에 따라 실행 여부 결정하도록 변경

--- src/main/java/com/example/market_follower/initializer/CandleInitializer.java
+++ src/main/java/com/example/market_follower/initializer/CandleInitializer.java
@@ -2,10 +2,14 @@ package com.example.market_follower.initializer;
 
 import com.example.market_follower.service.CandleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CandleInitializer implements ApplicationRunner {
@@ -16,7 +20,13 @@ public class CandleInitializer implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         candleService.initializeAllDailyCandleData();
         Thread.sleep(5000);
-        candleService.updateAllCandleData();
+
+        int currentHour = LocalDateTime.now().getHour();
+        if (currentHour <= 18) {
+            candleService.updateAllCandleData();
+        } else {
+            log.info(currentHour + "시는 실행 범위(0~18시)가 아니므로 updateAllCandleData 생략");
+        }
     }
 }
 
