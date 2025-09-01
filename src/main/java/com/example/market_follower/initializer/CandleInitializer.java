902135d3d210e3e445a9ddbda08e8b4516e0646a
seongjun-working-directories <seongjuncode@gmail.com>
2025-09-02 00:42:42 +0900

    |FEAT| 서버 초기화 시 코인 업데이트 포함

--- src/main/java/com/example/market_follower/initializer/CandleInitializer.java
+++ src/main/java/com/example/market_follower/initializer/CandleInitializer.java
@@ -1,6 +1,7 @@
 package com.example.market_follower.initializer;
 
 import com.example.market_follower.service.CandleService;
+import com.example.market_follower.service.MarketService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -15,18 +16,21 @@ import java.time.LocalDateTime;
 public class CandleInitializer implements ApplicationRunner {
 
     private final CandleService candleService;
+    private final MarketService marketService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        marketService.updateTradableCoinsInDb();
+        Thread.sleep(5000);
+
         candleService.initializeAllDailyCandleData();
         Thread.sleep(5000);
 
         int currentHour = LocalDateTime.now().getHour();
-        if (currentHour <= 18) {
+        if (currentHour <= 14) {
             candleService.updateAllCandleData();
         } else {
-            log.info(currentHour + "시는 실행 범위(0~18시)가 아니므로 updateAllCandleData 생략");
+            log.info(currentHour + "시는 실행 범위(0~14시)가 아니므로 updateAllCandleData 생략");
         }
     }
 }
-
